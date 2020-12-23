29a4ca524b6cb6c212c77b33e3158d9f4aecf845
xzllxz456 <xzllxz456@naver.com>
2020-12-23 20:01:13 +0900

    DbStudy_2020-12-23

--- DB/2020-12-23/15_유저생성_롤(ROLE)/01.유저생성_권한부여.sql
+++ DB/2020-12-23/15_유저생성_롤(ROLE)/01.유저생성_권한부여.sql
@@ -19,13 +19,26 @@ GRANT "RESOURCE" TO "MDGUEST" ;
 DROP USER 유저명 [CASCADE];
 --CASCADE : 삭제시점에 사용자(유저)가 보유한 모든 데이타 삭제
 *************************************/
+--(관리자계정 SYSTEM) 유저생성 권한을 가진 SYTTEM에서 작업
+-- 유저명 : MDGUEST, 암호 : mdguest
+CREATE USER "MDGUEST" IDENTIFIED BY "mdguest"
+DEFAULT TABLESPACE "USERS"
+TEMPORARY TABLESPACE "TEMP";
 
+-- 롤(역할) 부여 : GRANT
+GRANT "CONNECT" TO "MDGUEST" ;
+GRANT "RESOURCE" TO "MDGUEST" ;
+GRANT CONNECT, RESOURCE TO MDGUEST;
 
-
-
-
-
-
+-- 뷰 생성 권한 부여
+GRANT CREATE VIEW TO "MDGUEST" ;
+------------------
+--(SYSTEM) CONNECT, RESOUCE 롤(ROLE)에 있는 권한 확인
+SELECT *
+  FROM DBA_SYS_PRIVS
+ WHERE GRANTEE IN ('CONNECT', 'RESOURCE')
+ ORDER BY GRANTEE, PRIVILEGE
+;
 
 --=============================
 /****** 권한 부여(GRANT), 권한 취소(REVOKE) **********************
@@ -46,25 +59,47 @@ REVOKE 권한 [ON 객체] FROM {사용자|ROLE|PUBLIC,.., n} CASCADE
 REVOKE 권한 FROM 사용자; --권한을 사용자에게 부여
 REVOKE 권한 ON 객체 FROM 사용자; -객체에 대한 권한을 사용자에게 부여
 *************************************************/
---(관리자계정 SYSTEM) 유저생성 권한을 가진 SYSTEM에서 작업
--- 유저명MDGUEST 패스워드 : mdguest
--- USER SQL
-CREATE USER "MDGUEST" IDENTIFIED BY "mdguest"  
-DEFAULT TABLESPACE "USERS"
-TEMPORARY TABLESPACE "TEMP";
+--권한부여 : MADANG 유저의 BOOK 테이블에 대한 SELECT 권한을 MDGUEST에게 부여
+----(SYSTEM) 권한부여 - MADANG.BOOK
+SELECT * FROM MADANG.BOOK;
+GRANT SELECT ON MADANG.BOOK TO MDGUEST;
 
--- QUOTAS
+----(MDGUEST) MADANG.BOOK 테이블 데이터 조회(SELECT) 할 수 있음
+SELECT * FROM MADANG.BOOK; -- OK
+SELECT * FROM MADANG.CUSTOMER; --권한없어 접근불가 : ORA-00942: table or view does not exist
 
--- ROLES 롤(역활)부여 : GRANT
-GRANT "CONNECT" TO "MDGUEST" ;
-GRANT "RESOURCE" TO "MDGUEST" ;
-GRANT CONNECT, RESOURCE TO MDGUEST;
+----(SYSTEM) 권한 회수(REVOKE) - MADANG.BOOK
+REVOKE SELECT ON MADANG.BOOK FROM MDGUEST;
+--============================
+--(MADANG 유저에서) CUSTOMER 테이블에 대하여 MDGUEST 유저에게
+---- SELECT, UPDATE 권한 부여
+GRANT SELECT, UPDATE ON CUSTOMER TO MDGUEST;
 
--- 뷰 생성 권한 부여
-GRANT CREATE VIEW TO "MDGUEST" ;
--------------------------------------
---(SYSTEM) CONNECT, RESOURCE 롤(ROLE)에 있는 권한 확인
-SELECT *
- FROM DBA_SYS_PRIVS
- WHERE GRANTEE IN ('CONNECT', 'RESOURCE')
- ORDER BY GRANTEE, PRIVILEGE;
+---(MDGUEST 에서) CUSTOMER 테이블 SELECT, UPDATE
+--권한을 부여받아서 SELECT, UPDATE 작업 가능
+SELECT * FROM MADANG.CUSTOMER; 
+UPDATE MADANG.CUSTOMER
+   SET PHONE = '010-1111-2222'
+ WHERE NAME = '박세리'
+;
+-- 권한 취소 : REVOKE
+REVOKE SELECT, UPDATE ON CUSTOMER FROM MDGUEST;
+--------------------
+--(MADANG) WITH GRANT OPTION : 다른 유저에게 권한 부여할 수 있도록 허용
+GRANT SELECT, UPDATE ON CUSTOMER TO MDGUEST WITH GRANT OPTION;
+
+--(MDGUEST) 다른 유저에게 권한 부여
+GRANT SELECT, UPDATE ON MADANG.CUSTOMER TO HR;
+SELECT * FROM MADANG.CUSTOMER;
+
+--(HR) MADANG.CUSTOMER SELECT
+SELECT * FROM MADANG.CUSTOMER; --OK
+SELECT * FROM MADANG.BOOK; --ORA-00942: table or view does not exist
+
+--(MADANG) MDGUEST 권한회수 
+REVOKE SELECT, UPDATE ON CUSTOMER FROM MDGUEST;
+
+--=======================
+--(관리자 SYSTEM 에서) 유저삭제
+DROP USER MDGUEST CASCADE;
+
---
+++ DB/2020-12-23/16_SEQUENCE/시퀀스(SEQUENCE).sql
@@ -0,0 +1,44 @@
+/* **** 시퀀스(SEQUENCE)
+SEQUENCE : DB에서 제공하는 자동채번 객체
+생성 : CREATE SEQUENCE 시퀀스명;
+삭제 : DROP SEQUENCE 시퀀스명;
+
+시퀀스명.NEXTVAL : 시퀀스 번호(값) 생성
+시퀀스명.CURRVAL : 현재 시퀀스값 확인(NEXTVAL 한 번 이상 실행 후)
+****************************/
+
+CREATE SEQUENCE "SEQUENCE1" 
+MINVALUE 1 MAXVALUE 9999999999999999999999999999 
+INCREMENT BY 1
+START WITH 1
+CACHE 20 NOORDER  NOCYCLE;
+----------------------------
+SELECT SEQUENCE1.NEXTVAL FROM DUAL;
+------------------------------
+SELECT * FROM BOOK;
+SELECT MAX(BOOKID), MAX(BOOKID) +1  FROM BOOK;
+SELECT MAX(AAA), NVL(MAX(AAA),0) +1  FROM AAA; -- 데이터가 없을때
+-------------------
+-- BOOK 테이블에 INSERT 작업, BOOKID 최대값 +1
+INSERT INTO BOOK
+    (BOOKID, BOOKNAME, PUBLISHER, PRICE)
+VALUES ((SELECT NVL(MAX(BOOKID),0) + 1 FROM BOOK),
+        'MAX(BOOKID) + 1', 'ITBOOK', 20000);
+        COMMIT;
+SELECT * FROM BOOK ORDER BY BOOKID DESC;
+------------------------------------------------
+--시퀀스 사용(시작번호 지정 50, NOCACHE)
+CREATE SEQUENCE SEQ_BOOK
+START WITH 50
+INCREMENT BY 1 -- 생략시 동일 DEFAULT 1씩 증가
+NOCACHE -- 캐시 사용 안함
+;
+SELECT SEQ_BOOK.NEXTVAL FROM DUAL;
+-------------------------------
+-- BOOK 테이블에 시퀀스 사용 입력 처리
+INSERT INTO BOOK
+    (BOOKID, BOOKNAME, PUBLISHER, PRICE)
+VALUES (SEQ_BOOK.NEXTVAL,
+        'SEQ_BOOK.NEXTVAL 적용', 'ITBOOK', 20000);
+        COMMIT;
+SELECT * FROM BOOK ORDER BY BOOKID DESC;
